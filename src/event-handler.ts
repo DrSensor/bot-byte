69cd2f0097246116551eaaf1ed6aad700e608e98
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-12-18 06:23:06 +0700

    feat(lifecycle): add register event handler️ ⌛️
    🏁 **started** fire when bot is started
    🗣 **answered** fire everytime bot recieve an answer from a question
    ❔ **onAsking** fire when bot asking specific question
    ❗️ **onError** fire when error occur on specific event

---
+++ src/event-handler.ts
@@ -0,0 +1,32 @@
+type Response = string | void
+type Callback = (query?: string) => (Response | Promise<Response>)
+type EventHandler<T extends string> = { [key in T]?: Callback }
+
+export type Question = 'passphrase' | 'devicename'
+export type ErrorEvent = Question | keyof Event
+export interface Event {
+  started: Callback,
+  answered: Callback,
+  onAsking: EventHandler<Question>
+  onError: EventHandler<ErrorEvent>
+}
+
+// #region where the event handler being stored
+export const bot: Event = {
+  started: () => {},
+  answered: () => {},
+  onAsking: {},
+  onError: {}
+}
+// #endregion
+
+/**
+ * Register event handler
+ * each event represent the lifecycle of the bot
+ * and each handler support both normal and async function
+ */
+export default {
+  started: (callback: Callback) => (bot.started = callback),
+  onAsking: (question: Question, callback: Callback) => (bot.onAsking[question] = callback),
+  onError: (event: ErrorEvent, callback: Callback) => (bot.onError[event] = callback)
+}
--- src/shim-readline.ts
+++ src/shim-readline.ts
@@ -1,14 +1,26 @@
 import {ReadLineOptions} from 'readline'
 
+import {bot, Question} from './event-handler'
+
 /**
  * Shim readline.createInterface()
  * @see https://nodejs.org/api/readline.html#readline_readline_createinterface_options
  */
 export function createInterface(_options: ReadLineOptions) {
   return {
-    question(_query: string, answering: (response: string | void) => void) {
-      answering()
-    },
-    close() {}
+    close: bot.answered,
+    question(query: string, answering: (response: string | void) => void) {
+      // #region helpers
+      query = query.toLocaleLowerCase()
+      const on = (event: Question) => Promise.resolve(bot.onAsking[event]!())
+      const questionIncludes = (keywords: string[]) => keywords.every(keyword => query.includes(keyword))
+      // #endregion helpers
+
+      Promise.resolve(bot.started(query)).then(async () => {
+        if (questionIncludes(['passphrase'])) answering(await on('passphrase'))
+        else if (questionIncludes(['device', 'name'])) answering(await on('devicename'))
+        else answering()
+      }).catch(bot.onError.started)
+    }
   }
 }
