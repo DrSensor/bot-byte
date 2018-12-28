2c4f947cad5f14c5bc2588e9438d12b225c7f3ba
Fahmi Akbar Wildana <f.a.wildana@gmail.com>
2018-12-28 08:02:33 +0700

    feat(lib): group database, pairing, payment events

--- src/event-types.ts
+++ src/event-types.ts
@@ -1,3 +1,28 @@
+export interface IPairing {
+  attempt(from_address: string, pairing_secret: string): void
+  success(from_address: string, pairing_secret: string): void
+  remove(from_address: string): void
+}
+
+export interface IPayment {
+  send(unit: number, error: Error): void
+  recieve(arrNewUnits: number[]): void // #new_private_payment
+  confirm(arrUnits: number[]): void // #my_transactions_became_stable
+  'private:recieve'(// #received_payment
+    payer_device_address: string,
+    assocAmountsByAsset: string,
+    asset: string,
+    message_counter: number,
+    bToSharedAddress: string
+  ): void
+  'private:unhandle'(rowsLength: number): void // #unhandled_private_payments_left
+}
+
+export interface IDatabase {
+  'upgrade:start': void
+  'upgrade:finish': void
+  'cordova:ready': void
+}
 export interface IMessage {
   readonly from_address: string
   readonly text: string
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {EventEmitter} from 'events'
 import Typed from 'strict-event-emitter-types'
 
 import HWallet from './event-handler'
-import {IBot, IOption} from './event-types'
+import {IBot, IDatabase, IOption, IPairing, IPayment} from './event-types'
 import setOption from './options'
 
 type EventType<T> = Typed<EventEmitter, T>
@@ -13,6 +13,11 @@ export class Bot extends (EventEmitter as { new(): EventType<IBot> }) {
   static get instance() { return HWallet }
   static setOption = setOption
 
+  get database(): EventType<IDatabase> { return this._ev }
+  get pairing(): EventType<IPairing> { return this._ev }
+  get payment(): EventType<IPayment> { return this._ev }
+
+  private readonly _ev = new EventEmitter()
   private _device?: Device
   private _wallet?: Wallet
 
@@ -21,6 +26,20 @@ export class Bot extends (EventEmitter as { new(): EventType<IBot> }) {
     if (option) Bot.setOption(option)
 
     this.init().then(events => { // mapping events from https://developer.byteball.org/list-of-events
+
+      events.on('ready', () => this.database.emit('cordova:ready'))
+      events.on('started_db_upgrade', () => this.database.emit('upgrade:start'))
+      events.on('finished_db_upgrade', () => this.database.emit('upgrade:finish'))
+
+      events.on('pairing_attempt', (...r) => this.pairing.emit('attempt', r[0], r[1]))
+      events.on('paired', (...r) => this.pairing.emit('success', r[0], r[1]))
+      events.on('removed_paired_device', (...r) => this.pairing.emit('remove', r[0]))
+
+      events.on('new_private_payment', (...r) => this.payment.emit('recieve', r[0]))
+      events.on('my_transactions_became_stable', (...r) => this.payment.emit('confirm', r[0]))
+      events.on('received_payment', (...r) => this.payment.emit('private:recieve', r[0], r[1], r[2], r[3], r[4]))
+      events.on('unhandled_private_payments_left', (...r) => this.payment.emit('private:unhandle', r[0]))
+
       events.on('text', (...r) => this.emit('message', {
         from_address: r[0],
         text: r[1],
@@ -33,6 +52,13 @@ export class Bot extends (EventEmitter as { new(): EventType<IBot> }) {
   sendMessage(to_address: string, message: string) {
     if (!this._device) throw new Error('Bot not ready')
     else this._device.sendMessageToDevice(to_address, 'text', message)
+  }
+
+  sendPayment(recipient_address: string, amount: number) {
+    if (!this._wallet) throw new Error('Bot not ready')
+    else this._wallet.issueChangeAddressAndSendPayment(null, amount, recipient_address, undefined,
+      (err: Error, unit: number) => this.payment.emit('send', unit, err)
+    )
   }
 
   private async init() {
